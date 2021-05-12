b4064e80caad2d5d1e2105e519074e124be1d824
alexandr <hehehhahahahyhy@gmail.com>
2021-05-12 22:24:42 +0300

    first

---
+++ main.js
@@ -0,0 +1,149 @@
+// "use strict"
+// console.log('Hello');
+// console.log(3 + 
+// 1
+// +2);
+// [1, 2].forEach((item) => console.log(item));
+// console.log('Hello');
+
+// var userName = 'John'
+
+// let name = 'Alexandr';
+// let admin = name;
+// console.log(name, admin);
+
+// let str = "Double quotes";
+// let str1 = 'Single quotes';
+// let str2 = `Lorem ipsum dolor sit amet consectetur.
+// ${str} ${1 + 2}`;
+ 
+// console.log(str, str1, str2);
+// console.log('str' / 2);
+
+// let number = 1;
+// let string = 'String';
+// let boolean = true;
+// let name = null;
+// let name1;
+// let symbol = Symbol('СИМВОЛ')
+// const bigInt = 945813490582240385254645n;
+// const obj = {
+
+// };
+// const arr = [
+
+// ];
+// console.log(typeof(number));
+// console.log(typeof(string));
+// console.log(typeof(boolean));
+// console.log(typeof(name));
+// console.log(typeof(name1));
+// console.log(typeof(symbol));
+// console.log(typeof(bigInt));
+// console.log(typeof(obj));
+// console.log(typeof(arr));
+
+// alert('hello');
+// confirm('yes or no');
+
+// let age = prompt('Скольколь тебе лет?');
+// alert(age);
+
+// let value = true;
+// alert(typeof value);
+// value = String(value);
+// alert(typeof value);
+
+// let firstNum = '10';
+// let secondNum = '5';
+// let res = firstNum + secondNum;
+// console.log(res);
+
+// let age = parseInt('12.3wew');
+// console.log(age);
+
+// console.log(Boolean(1) );
+// console.log(Boolean(0) );
+// console.log(Boolean("Hello!") );
+// console.log(Boolean("") );
+
+
+// console.log(2 **3);
+// console.log( 3 % 8);
+// console.log( 5 % 3);
+// console.log( 6 % 3);
+// console.log(2 + '3');
+// console.log('7' + 2);
+// console.log(2 + 2 + '3');
+
+// console.log(6 - '2');
+// console.log(+'2' + +'25');
+// console.log(+true);
+
+// let counter = 16;
+// console.log(++counter);
+
+// let counter = 1;
+// console.log(counter++);
+ 
+
+
+
+// let firstNumb = prompt('Ведите первое число');
+// let secondNum = prompt('Ведите второе число');
+// alert ('Сумма ваших чисел равна:' + (firstNumb **2 + secondNum**2));
+ 
+
+// let username = 'sasha';
+
+// if(username === 'jon') {
+//     console.log(username)
+// } else if(username === 'Tom') { 
+//     console.log('Tom')
+// } else {
+//     console.log('sasha')
+// };
+
+// let firstNum = prompt('Введите число');
+// let secondNum = firstNum % 5;
+// let thirdNum = firstNum % 3;
+
+// if(secondNum == 0 && thirdNum == 0) {
+//     alert('FizzBuzz')
+// } else if(thirdNum === 0) {
+//     alert('Buzz')
+// } else if(secondNum === 0) {
+//         alert('Fizz')
+// } else {
+//     alert('ERROR')
+// };
+
+
+// let num = 0
+// while( num < 5 ) {
+//     console.log (num)
+//     num++
+// };
+
+// let i = 0;
+// do {
+// console.log( 4 )
+// i++;
+// } while (i < 3);
+
+// for( let i = 2; i < 11; i++) {
+//     if (i % 2){
+//       continue;
+//     }
+//     console.log(i);
+// };
+
+let num = prompt('Ведитете число больше 100');
+while (num < 100){
+    num = prompt('Ведитете другое число ещё раз');
+    console.log (num);
+};
+
+
+
+
